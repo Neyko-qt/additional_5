0242126162ac88fd09a8065322fcb0d84443ae53
Neyko-qt <fauster08@mail.ru>
2018-02-28 23:11:55 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    var openedBracets = [];
+    var closedBracets = [];
+
+    if (str.length % 2 == 1) {
+      return false;
+    }
+    
+    var result = [];
+
+    for (var  i = 0; i < bracketsConfig.length; i++) {
+      openedBracets.push(bracketsConfig[i][0]);
+      closedBracets.push(bracketsConfig[i][1]);
+    }
+
+    for (var i = 0; i < str.length ; i++) {
+        
+      if (~openedBracets.indexOf(str[i])) {
+        result.push(str[i]);
+      }
+
+      if (~closedBracets.indexOf(str[i])) {
+        
+        if (closedBracets.indexOf(str[i]) == openedBracets.indexOf(result[result.length-1])) {
+          result.pop();
+        }
+
+      }
+    }
+
+    if (result.length == 0){
+      return true;
+    }
+    
+    return false;
 }
